33c50993daf9839aba4bcd518bbb172211369c6f
syed dilawar <wasimdilawer@gmail.com>
2020-11-25 15:17:39 -0800

    working on it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
-
-import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <Router>
+      <div>
+        <NavTabs />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
   );
 }
 
