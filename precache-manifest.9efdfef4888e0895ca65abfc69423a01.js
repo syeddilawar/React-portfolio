62fdc9a3613fc6b5a4a51873ce4c7770ebf5f068
syed dilawar <wasimdilawer@gmail.com>
2020-12-14 14:44:46 -0800

    Updates

---
+++ precache-manifest.9efdfef4888e0895ca65abfc69423a01.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96bf53ce08a8bbbd7ad7c4d69d9339d2",
+    "url": "/syeddilawar/react-portfolio/index.html"
+  },
+  {
+    "revision": "72755e6dd9eeb673d49a",
+    "url": "/syeddilawar/react-portfolio/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "fddec22585787d526efe",
+    "url": "/syeddilawar/react-portfolio/static/css/main.2f9047d8.chunk.css"
+  },
+  {
+    "revision": "72755e6dd9eeb673d49a",
+    "url": "/syeddilawar/react-portfolio/static/js/2.c52d0866.chunk.js"
+  },
+  {
+    "revision": "12393ff7ff42d28b511204d747ac4732",
+    "url": "/syeddilawar/react-portfolio/static/js/2.c52d0866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fddec22585787d526efe",
+    "url": "/syeddilawar/react-portfolio/static/js/main.04cd8b75.chunk.js"
+  },
+  {
+    "revision": "b4c0752a40bc499bee16",
+    "url": "/syeddilawar/react-portfolio/static/js/runtime-main.d04b7864.js"
+  },
+  {
+    "revision": "66efa9ef9943bfacba3634fac666bc3a",
+    "url": "/syeddilawar/react-portfolio/static/media/code-quiz.66efa9ef.png"
+  },
+  {
+    "revision": "77d83271121fa4d01a67f62e434aa711",
+    "url": "/syeddilawar/react-portfolio/static/media/code.77d83271.jpg"
+  },
+  {
+    "revision": "0924c142a5dc2a9cbd16a5339fe6646b",
+    "url": "/syeddilawar/react-portfolio/static/media/dayplanner.0924c142.png"
+  },
+  {
+    "revision": "02e5953e6ea9b9de7f10a007063a62b8",
+    "url": "/syeddilawar/react-portfolio/static/media/employee.02e5953e.gif"
+  },
+  {
+    "revision": "f9ac1c512e123d7f644bc97b8947ab92",
+    "url": "/syeddilawar/react-portfolio/static/media/generater.f9ac1c51.gif"
+  },
+  {
+    "revision": "fae7c53da6df3a0c37d541d48f6b14eb",
+    "url": "/syeddilawar/react-portfolio/static/media/mypic.fae7c53d.jpeg"
+  },
+  {
+    "revision": "9e1c5322bde6deb0ab353f7da13e8e57",
+    "url": "/syeddilawar/react-portfolio/static/media/weatherdashboard.9e1c5322.png"
+  }
+]);
